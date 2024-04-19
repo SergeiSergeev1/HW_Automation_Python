4302c02f888afe8b5c03a5a46f4724f406392949
SergeiSergeev1 <s-a.sergeev@mail.ru>
2024-04-19 10:37:44 +0300

    HW5

--- Lesson_5/Клик_по_кнопке_без_ID_chrome.py
+++ Lesson_5/Клик_по_кнопке_без_ID_chrome.py
@@ -11,7 +11,7 @@ for i in range(3):
     sleep(1)
     click_button = driver.find_element(By.CSS_SELECTOR, "button.btn.btn-primary").click()
     sleep(1)
-    driver.send
+    
    
 
 driver.quit()
--- Lesson_5/Клик_по_кнопке_без_ID_firefox.py
+++ Lesson_5/Клик_по_кнопке_без_ID_firefox.py
@@ -20,4 +20,3 @@ driver.quit()
 
 
 
-driver.close()
